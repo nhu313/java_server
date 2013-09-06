46823ee06e2bd479e6cda399c88c17eb9a360ef0
Nhu Nguyen <nhu313@gmail.com>
2013-09-06 08:59:00 -0500

    cleaned up code and removed logs from Server

--- src/server/request/processor/DirectoryReader.java
+++ src/server/request/processor/DirectoryReader.java
@@ -12,7 +12,6 @@ public class DirectoryReader {
         String private_dir_name = Config.PRIVATE_PATH.substring(1);
         File dir = new File(Config.DIRECTORY_PATH);
         List<String> files = new ArrayList<String>();
-        System.out.println(dir.exists());
         for (String fileName : dir.list()){
             if (!private_dir_name.equals(fileName)){
                 files.add(fileName);
