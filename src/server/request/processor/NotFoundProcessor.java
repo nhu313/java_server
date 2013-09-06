815e680a2d7838ebb8361ee0343202bea9ecc1e4
Nhu Nguyen <nhu313@gmail.com>
2013-09-06 10:26:33 -0500

    cleaned up import

--- src/server/request/processor/NotFoundProcessor.java
+++ src/server/request/processor/NotFoundProcessor.java
@@ -1,5 +1,6 @@
 package server.request.processor;
 
+import server.Logger;
 import server.Request;
 import server.Response;
 import server.ResponseCode;
@@ -7,7 +8,7 @@ import server.ResponseCode;
 public class NotFoundProcessor implements Processor{
     @Override
     public Response process(Request request) {
-        server.Logger.info(request.getHttpMethod() + " " + request.getPath() + " HTTP/1.1");
+        Logger.info(request.getHttpMethod() + " " + request.getPath() + " HTTP/1.1");
         return new Response(ResponseCode.NOT_FOUND);
     }
 }
